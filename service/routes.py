16446b50d27361e5f8f8ea8a57a676ab38f12797
BAGAS SEBASTIAN <111202214715@mhs.dinus.ac.id>
2026-05-10 08:43:53 +0700

    update product endpoint

--- service/routes.py
+++ service/routes.py
@@ -132,8 +132,15 @@ def update_product(product_id):
     # validate /product_id
     if product_id == 0 or product_id is None:
         return {'message': 'product /id not set'}, status.HTTP_400_BAD_REQUEST
+    found = Product.find(product_id)
+    if found is None:
+        abort(status.HTTP_404_NOT_FOUND)
+    
     payload = request.get_json()
-    return {'message':'ok'}, status.HTTP_200_OK
+    found.deserialize(payload)
+    found.update()
+    return found.serialize(), status.HTTP_200_OK
+
 ######################################################################
 # D E L E T E   A   P R O D U C T
 ######################################################################
